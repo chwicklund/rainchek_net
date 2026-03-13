17dbba9fc7c413ad34770f6ff828ba492c43429a
chwicklund <chwicklund@gmail.com>
2026-03-13 00:34:49 +0000

    V1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import json
 from jinja2 import Environment, FileSystemLoader
 from flask_frozen import Freezer
@@ -16,13 +16,11 @@ def index():
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template("index.html.jinja2")
 
-    ## FOR CONTACT US PAGE, ADD DROPDOWN FOR SUBJECT LINE, INQUIRY VS SUPPORT
-
     output = template.render(**context)
 
     return output
 
-@app.route('/privacy')
+@app.route('/privacy.html')
 def privacy():
     with open('config/context.json') as f:
         context = json.load(f)
@@ -30,15 +28,53 @@ def privacy():
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template("privacy.html.jinja2")
 
-    ## FOR CONTACT US PAGE, ADD DROPDOWN FOR SUBJECT LINE, INQUIRY VS SUPPORT
+    output = template.render(**context)
+
+    return output
+
+
+@app.route('/about.html')
+def about():
+    with open('config/context.json') as f:
+        context = json.load(f)
+
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template("about.html.jinja2")
 
     output = template.render(**context)
 
     return output
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+@app.route('/faq.html')
+def faq():
+    with open('config/context.json') as f:
+        context = json.load(f)
+
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template("faq.html.jinja2")
+
+    output = template.render(**context)
+
+    return output
+
+
+# Add a freezer generator for all routes
+@freezer.register_generator
+def url_generator():
+    yield url_for('index')
+    yield url_for('privacy')
+    yield url_for('about')
+    yield url_for('faq')
+
 
 if __name__ == '__main__':
-    # Use the freezer if run directly
-    freezer.freeze()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--freeze', action='store_true')
+    args = parser.parse_args()
+
+    if args.freeze:
+        freezer.freeze()
+    else:
+        app.run(debug=True)
